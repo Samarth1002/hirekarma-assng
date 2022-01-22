eb10cfb6cc2dd2597309011a8cd142bea15c68cb
Samarth1002 <samarthanand622001@gmail.com>
2022-01-22 12:32:37 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+    </>
+  );
+}
+
+export default App;
